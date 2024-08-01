76ba829a451f74eca272c9a053d2a138ba82c285
Hisui <i@hisui.io>
2024-08-02 00:34:54 +1000

    deeplab.py get_miou.py train.py utils.py utils_metrics.py

--- utils/utils.py
+++ utils/utils.py
@@ -6,44 +6,34 @@ import torch
 from PIL import Image
 
 
-#---------------------------------------------------------#
-#   将图像转换成RGB图像，防止灰度图在预测时报错。
-#   代码仅仅支持RGB图像的预测，所有其它类型的图像都会转化成RGB
-#---------------------------------------------------------#
 def cvtColor(image):
     if len(np.shape(image)) == 3 and np.shape(image)[2] == 3:
-        return image 
+        return image
     else:
         image = image.convert('RGB')
-        return image 
+        return image
 
-#---------------------------------------------------#
-#   对输入图像进行resize
-#---------------------------------------------------#
+
 def resize_image(image, size):
-    iw, ih  = image.size
-    w, h    = size
+    iw, ih = image.size
+    w, h = size
 
-    scale   = min(w/iw, h/ih)
-    nw      = int(iw*scale)
-    nh      = int(ih*scale)
+    scale = min(w / iw, h / ih)
+    nw = int(iw * scale)
+    nh = int(ih * scale)
 
-    image   = image.resize((nw,nh), Image.BICUBIC)
-    new_image = Image.new('RGB', size, (128,128,128))
-    new_image.paste(image, ((w-nw)//2, (h-nh)//2))
+    image = image.resize((nw, nh), Image.BICUBIC)
+    new_image = Image.new('RGB', size, (128, 128, 128))
+    new_image.paste(image, ((w - nw) // 2, (h - nh) // 2))
 
     return new_image, nw, nh
-    
-#---------------------------------------------------#
-#   获得学习率
-#---------------------------------------------------#
+
+
 def get_lr(optimizer):
     for param_group in optimizer.param_groups:
         return param_group['lr']
 
-#---------------------------------------------------#
-#   设置种子
-#---------------------------------------------------#
+
 def seed_everything(seed=11):
     random.seed(seed)
     np.random.seed(seed)
@@ -53,18 +43,18 @@ def seed_everything(seed=11):
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
-#---------------------------------------------------#
-#   设置Dataloader的种子
-#---------------------------------------------------#
+
 def worker_init_fn(worker_id, rank, seed):
     worker_seed = rank + seed
     random.seed(worker_seed)
     np.random.seed(worker_seed)
     torch.manual_seed(worker_seed)
 
+
 def preprocess_input(image):
     image /= 255.0
     return image
+
 
 def show_config(**kwargs):
     print('Configurations:')
@@ -75,19 +65,21 @@ def show_config(**kwargs):
         print('|%25s | %40s|' % (str(key), str(value)))
     print('-' * 70)
 
+
 def download_weights(backbone, model_dir="./model_data"):
     import os
     from torch.hub import load_state_dict_from_url
-    
+
     download_urls = {
-        'mobilenet' : 'https://github.com/bubbliiiing/deeplabv3-plus-pytorch/releases/download/v1.0/mobilenet_v2.pth.tar',
-        'xception'  : 'https://github.com/bubbliiiing/deeplabv3-plus-pytorch/releases/download/v1.0/xception_pytorch_imagenet.pth',
+        'mobilenet': 'https://github.com/bubbliiiing/deeplabv3-plus-pytorch/releases/download/v1.0/mobilenet_v2.pth.tar',
+        'xception': 'https://github.com/bubbliiiing/deeplabv3-plus-pytorch/releases/download/v1.0/xception_pytorch_imagenet.pth',
     }
     url = download_urls[backbone]
-    
+
     if not os.path.exists(model_dir):
         os.makedirs(model_dir)
     load_state_dict_from_url(url, model_dir)
+
 
 class EarlyStopping:
     def __init__(self, patience=7, verbose=False, delta=0):
@@ -121,9 +113,8 @@ class EarlyStopping:
             self.counter = 0
 
     def save_checkpoint(self, val_loss, model, save_dir):
-        '''验证损失减少时保存模型'''
         if self.verbose:
-            print(f'验证损失减少 ({self.val_loss_min:.6f} --> {val_loss:.6f}).  保存模型 ...')
+            print(f'validation loss decrease ({self.val_loss_min:.6f} --> {val_loss:.6f}).  saving model ...')
         # 保存模型的代码
         torch.save(model.state_dict(), os.path.join(save_dir, "last_epoch_weights.pth"))
         self.val_loss_min = val_loss
--- utils/utils_fit.py
+++ utils/utils_fit.py
@@ -33,18 +33,12 @@ def fit_one_epoch(model_train, model, loss_history, eval_callback, optimizer, ep
                 pngs    = pngs.cuda(local_rank)
                 labels  = labels.cuda(local_rank)
                 weights = weights.cuda(local_rank)
-        #----------------------#
-        #   清零梯度
-        #----------------------#
+
         optimizer.zero_grad()
         if not fp16:
-            #----------------------#
-            #   前向传播
-            #----------------------#
+
             outputs = model_train(imgs)
-            #----------------------#
-            #   计算损失
-            #----------------------#
+
             if focal_loss:
                 loss = Focal_Loss(outputs, pngs, weights, num_classes = num_classes)
             else:
@@ -55,26 +49,18 @@ def fit_one_epoch(model_train, model, loss_history, eval_callback, optimizer, ep
                 loss      = loss + main_dice
 
             with torch.no_grad():
-                #-------------------------------#
-                #   计算f_score
-                #-------------------------------#
+
                 _f_score = f_score(outputs, labels)
 
-            #----------------------#
-            #   反向传播
-            #----------------------#
+
             loss.backward()
             optimizer.step()
         else:
             from torch.cuda.amp import autocast
             with autocast():
-                #----------------------#
-                #   前向传播
-                #----------------------#
+
                 outputs = model_train(imgs)
-                #----------------------#
-                #   计算损失
-                #----------------------#
+
                 if focal_loss:
                     loss = Focal_Loss(outputs, pngs, weights, num_classes = num_classes)
                 else:
@@ -85,14 +71,10 @@ def fit_one_epoch(model_train, model, loss_history, eval_callback, optimizer, ep
                     loss      = loss + main_dice
 
                 with torch.no_grad():
-                    #-------------------------------#
-                    #   计算f_score
-                    #-------------------------------#
+
                     _f_score = f_score(outputs, labels)
                     
-            #----------------------#
-            #   反向传播
-            #----------------------#
+
             scaler.scale(loss).backward()
             scaler.step(optimizer)
             scaler.update()
@@ -125,13 +107,9 @@ def fit_one_epoch(model_train, model, loss_history, eval_callback, optimizer, ep
                 labels  = labels.cuda(local_rank)
                 weights = weights.cuda(local_rank)
 
-            #----------------------#
-            #   前向传播
-            #----------------------#
+
             outputs     = model_train(imgs)
-            #----------------------#
-            #   计算损失
-            #----------------------#
+
             if focal_loss:
                 loss = Focal_Loss(outputs, pngs, weights, num_classes = num_classes)
             else:
@@ -140,9 +118,7 @@ def fit_one_epoch(model_train, model, loss_history, eval_callback, optimizer, ep
             if dice_loss:
                 main_dice = Dice_loss(outputs, labels)
                 loss  = loss + main_dice
-            #-------------------------------#
-            #   计算f_score
-            #-------------------------------#
+
             _f_score    = f_score(outputs, labels)
 
             val_loss    += loss.item()
@@ -162,9 +138,7 @@ def fit_one_epoch(model_train, model, loss_history, eval_callback, optimizer, ep
         print('Epoch:'+ str(epoch + 1) + '/' + str(Epoch))
         print('Total Loss: %.3f || Val Loss: %.3f ' % (total_loss / epoch_step, val_loss / epoch_step_val))
         
-        #-----------------------------------------------#
-        #   保存权值
-        #-----------------------------------------------#
+
         if (epoch + 1) % save_period == 0 or epoch + 1 == Epoch:
             torch.save(model.state_dict(), os.path.join(save_dir, 'ep%03d-loss%.3f-val_loss%.3f.pth' % (epoch + 1, total_loss / epoch_step, val_loss / epoch_step_val)))
 
